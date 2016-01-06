3558d3482634e4ff6afe9eda0b3210dbe2ec47c5
baishuai <baishuai900124@163.com>
2016-01-06 21:19:01 +0800

    添加node服务器

---
+++ app.js
@@ -0,0 +1,42 @@
+/**
+ * Created by baishuai on 2016/1/6.
+ */
+
+'use strict';
+
+var http    = require('http'),
+    express = require('express'),
+
+    app     = express(),
+
+    server  = http.createServer(app);
+
+app.configure(function () {
+    app.use( express.bodyParser() );
+    app.use( express.methodOverride() );
+    app.use( express.static(__dirname + '/public') );
+    app.use( app.router );
+});
+
+app.configure('development', function () {
+    app.use( express.logger() );
+    app.use( express.errorHandler({
+        dumpExceptions : true,
+        showStack      : true
+    }) );
+});
+
+app.configure('production', function () {
+    app.use( express.errorHandler() );
+});
+
+app.get('/', function ( request, response ) {
+    response.redirect( '/spa.html ');
+});
+
+server.listen( 3000 );
+console.log(
+    'Express server listening on port %d in %s mode',
+    server.address().port, app.settings.env
+);
+
